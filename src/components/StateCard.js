d72574b384b7159c6ff4f19b87703bb9f3cbf15b
vidallizet <lvidalsaldivar7855@richmondhillhs.org>
2022-02-10 04:06:51 +0000

    buttonchanges

--- src/components/StateCard.js
+++ src/components/StateCard.js
@@ -13,10 +13,35 @@ class StateCard extends Component {
 
   // Functions to handle button presses.
   handleButton1 = () => {
-    alert("Button 1 pressed!");
+  
+  if (this.state.button1 === "On!"){
+      this.setState({button1: "Off!"})
+  }      
+      else{
+     this.setState({button1: "On!"})
+    }
   }
-  handleButton2 = () => {}
-  handleButton3 = () => {}
+
+  
+  handleButton2 = () => {
+this.setState(state => {
+    state.button2 = state.button2 + 1
+    return state
+})      
+  }
+  handleButton3 = () => {
+      if(this.state.button3 === "You"){
+          this.setState({button3: "are"})
+      }
+          else{
+              this.setState({button3: "cool!"})
+          }
+
+      if(this.state.button3 === "cool!"){
+        this.setState({button3: "You"})
+    }
+
+  }
 
   // The render function
   render() {
@@ -31,14 +56,14 @@ class StateCard extends Component {
             <p>Current status: {this.state.button1}</p>
           </div>
           <div className="item">
-            <button onClick="FIX_THIS_EVENT">
+            <button onClick={this.handleButton2}>
               Button 2
             </button>
-            <p>Current count: ADD_STATE_HERE</p>
+            <p>Current count: {this.state.button2}</p>
           </div>
           <div className="item">
-            <button onClick="FIX_THIS_EVENT">button 3</button>
-            <p>Current word: ADD_STATE_HERE</p>
+            <button onClick={this.handleButton3}>button 3</button>
+            <p>Current word:{this.state.button3}</p>
           </div>
         </div>
       </div>
